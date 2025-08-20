9253c24669babd963260e715914d45caad30d979
Vovas <vovaafon25@gmail.com>
2025-08-20 13:30:55 +0300

    Implemented tag list

--- todo_list/views.py
+++ todo_list/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from todo_list.forms import TaskCreateForm
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -34,3 +34,23 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("list:task-list")
 
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("list:tag-list")
